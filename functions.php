29316431ebe5d12c1406baf6886271471ae8e75f
maratustra <maratustraa@gmail.com>
2021-07-14 16:59:15 +0500

    перенесли верстку со стилями и js файлом на wordpress

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+// правильный способ подключить стили и скрипты
+add_action( 'wp_enqueue_scripts', 'realestate_scripts' );
+// add_action('wp_print_styles', 'theme_name_scripts'); // можно использовать этот хук он более поздний
+function realestate_scripts() {
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/style.min.css');
+  wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.min.js', array(), '1.0.0', true );
+}
